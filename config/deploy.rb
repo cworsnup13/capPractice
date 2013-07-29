c83cbbf01aa4bcf004326499223f327c5502771a
cworsnup <calvin.worsnup@blackboard.com>
2013-07-29 15:42:38 -0700

    deploy from git

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,13 @@
-#set :application, "set your application name here"
+set :application, "webcapistrano"
 #set :repository,  "set your repository location here"
 #
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+#set :user, 'deploy'
+
 #role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-#role :app, "your app-server here"                          # This may be the same as your `Web` server
+role :app, "localhost"                          # This may be the same as your `Web` server
 #role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
@@ -25,6 +27,26 @@
 # end
 load "config/shared/custom_config"
 
-task :testingTouches do
-  run "touch /tmp/testinTouch.txt"
+default_run_options[:pty]=true
+set :captain_home, "/home/cworsnup/projects/kombat/current"
+set :repository, "git@github.com:bbmobile/webcapistrano.git"
+set :deploy_to, "/home/cworsnup/projects/kombat"
+set :ssh_options, { :forward_agent => true }
+
+task :update, roles => [:app] do
+  transaction do
+    update_code
+    symlink
+  end
 end
+
+task :update_code, roles => [:app] do
+  run "git clone #{repository} #{release_path}"
+  #run "mkdir #{release_path}/tmp"
+end
+
+task :symlink, roles => [:app] do
+  sudo "ln -nfs #{release_path} #{captain_home}"
+end
+
+
